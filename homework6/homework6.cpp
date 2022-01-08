a06532e2c58d02fc2eb5a667382373231f9aba3f
leanhuynh2002 <20120028@student.hcmus.edu.vn>
2022-01-08 08:24:40 +0700

    commented

--- homework6/homework6.cpp
+++ homework6/homework6.cpp
@@ -9,15 +9,20 @@ int main()
     try {
         TrieNode* Dic = NULL;
 
+        // demo function createTries
         createTrie(Dic, "HW6-dic.txt");
         
+        // demo function lookUp
         string word = "friend";
         cout << "Gia tri cua tu friend : " << lookUp(Dic, word) << endl;
+        // demo function remove
         Remove(Dic, word);
         cout << "Gia tri cua tu friend : " << lookUp(Dic, word) << endl;
+        // demo function Insert
         Insert(Dic, "vtpt", 2000);
         cout << "Gia tri cua tu vtpt moi them vao: " << lookUp(Dic, "vtpt") << endl;
 
+        // demo function lookUpPrefix
         do {
             cout << "Nhap prefix can tra: ";
             getline(cin, word);
@@ -28,6 +33,7 @@ int main()
             cout << "\n---------------------------------------------------\n\n";
         } while (word != "stop");
 
+        // delete all node
         deleteTrieNode(Dic);
     }
     catch (const char* message) {
