7e1b7bf6550781488e37e9ca440c436293f85b36
leanhuynh2002 <20120028@student.hcmus.edu.vn>
2022-01-09 11:05:45 +0700

    has changed

--- homework8/LZW.cpp
+++ homework8/LZW.cpp
@@ -51,6 +51,7 @@ vector<newUNICODE> convertArraytToVector(int n, char** str)
 
 // print the compression ratio
 // take the number of bits is 9
+// the compressionRatio is decimal round to 2
 void printTheCompressionRatio(const int before, const int after)
 {
 	double ptemp = 1 - double((double(after * 9) / double(before * 8)));
--- homework8/main.cpp
+++ homework8/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char** argv)
 	// notice that example string: ABCDEFGHIJKLMNOPQRSTUVWXYZ has 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90
 	// we see no newUNICODE has over than 255 were used and the bit before 26 * 8 and after 26 * 9 (beacause newUNICODE has number of bits is 9)
 	// so that the compression ratio can be a negative number
+	// and the compression ratio is the decimal round to 2
 	try {
 		if (argc < 2) {
 			throw "Error form input data !\n";
